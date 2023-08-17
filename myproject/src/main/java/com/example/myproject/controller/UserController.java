61dd11a110e80787b6f74d2c76b2839687066220
Fjq11 <1072335602@qq.com>
2023-08-17 22:35:09 +0800

    update pull

--- myproject/src/main/java/com/example/myproject/controller/UserController.java
+++ myproject/src/main/java/com/example/myproject/controller/UserController.java
@@ -1,13 +1,31 @@
 package com.example.myproject.controller;
 
 
+import com.example.myproject.entity.User;
+import com.example.myproject.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserServiceImpl userService;
+
+    @RequestMapping("add")
+    public User add(User user){
+        userService.save(user);
+        return  user;
+    }
+
+    @GetMapping("list")
+    public List<User> list() {
+        return userService.list();
+    }
 
 }
---
+++ myproject/src/main/java/com/example/myproject/dao/UserRepository.java
@@ -0,0 +1,23 @@
+package com.example.myproject.dao;
+
+import com.example.myproject.entity.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserRepository {
+
+
+    private List<User> userDemoList = new ArrayList<>();
+
+    public void save(User user) {
+        userDemoList.add(user);
+    }
+
+    public List<User> findAll() {
+        return userDemoList;
+    }
+
+}
